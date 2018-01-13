1f2c27375326befdc75d6b0cd493398bd1e2889a
fcu-d0514534 <D0514534@gmail.com>
2018-01-13 21:26:42 +0800

    DEMO

--- app/src/main/java/com/shang/softdemo/MainActivity.java
+++ app/src/main/java/com/shang/softdemo/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//89
         listView=(ListView)findViewById(R.id.list);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
